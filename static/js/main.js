e6f1ed6fd162f1a475b9b01fdc532fbd30c97582
leoendless <liulu897292197@qq.com>
2018-02-08 18:15:24 +0800

    upgrade section 1

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,19 @@
-(function () {
-  AOS.init({
-    easing: 'ease-in-out',
-    duration: 600
-  });
-})()
+(function ($) {
+  var $win = $(window);
+  var $header = $('nav.header');
+
+  function init() {
+    AOS.init({
+      easing: 'ease-in-out',
+      duration: 600
+    });
+
+    window.onscroll = scrolHandler;
+  }
+
+  function scrolHandler() {
+    $header.toggleClass('header-fix', $win.scrollTop() > 100);
+  }
+
+  init();
+})(jQuery);
