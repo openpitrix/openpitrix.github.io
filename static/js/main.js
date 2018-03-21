2b968ff15c4add4a1dc2bfd9039f1c82dad7ab4c
libangrui <librf2e@gmail.com>
2018-03-21 15:15:50 +0800

    add mobile homepage

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 (function ($) {
   var $win = $(window);
   var $header = $('nav.header');
+  var $menuBtn = $('.menu-btn');
 
   function init() {
     AOS.init({
@@ -13,11 +14,27 @@
     new SmoothScroll('a[href*="#"]');
 
     window.onscroll = scrolHandler;
+
+    $menuBtn.on('click', function() {
+      $header.toggleClass('active');
+    });
   }
 
   function scrolHandler() {
     $header.toggleClass('header-fix', $win.scrollTop() > 100);
   }
+
+  !(function(doc, win) {
+      var docEle = doc.documentElement;
+      var event = 'onorientationchange' in window ? 'orientationchange' : 'resize';
+      var fn = function() {
+        var width = docEle.clientWidth;
+        width && (docEle.style.fontSize = 100 * (width / 375) + "px");
+      };
+
+    win.addEventListener(event, fn, false);
+    doc.addEventListener('DOMContentLoaded', fn, false);
+  }(document, window));
 
   init();
 })(jQuery);
